35bbe09106a56d1880223df76bdeb650385d35c7
Shreya Kumari <96626023+Shreya-Kumari21@users.noreply.github.com>
2022-06-24 12:57:32 +0530

    Add files via upload

---
+++ numbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class number{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a Number");
+		
+		int num=sc.nextInt();
+		int counter=0;
+		
+		for(int i=2;i<num;i++){
+			if(num%i==0){
+				counter++;
+			}
+		}
+			
+		if(counter==0){
+			System.out.println("Prime");
+		}else{
+			System.out.println("Not Prime");
+		}
+		//System.out.println(num);
+	}
+}
