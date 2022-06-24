76ce67fa824ab43980da9a92679ded916f06a811
Shreya Kumari <96626023+Shreya-Kumari21@users.noreply.github.com>
2022-06-24 15:33:16 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+class square{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter a Number: ");
+		int num=sc.nextInt();
+		int total=0;
+		int rem;
+		while(num>0)
+		{
+		    rem=num%10;
+			total=total+(rem*rem);
+			num=num/10;
+		}
+		System.out.println(total);
+	}
+}
